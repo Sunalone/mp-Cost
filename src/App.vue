cae9617ddcc5fb3c963d44939abdca1ce42e1206
Sunalone <843258092@qq.com>
2023-11-11 11:56:26 +0800

    feat:Description:新增搜索框

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,51 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+    import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
 
-onLaunch(() => {
-  console.log('App Launch')
-})
-onShow(() => {
-  console.log('App Show')
-})
-onHide(() => {
-  console.log('App Hide')
-})
+    onLaunch(() => {
+        console.log('App Launch')
+    })
+    onShow(() => {
+        console.log('App Show')
+    })
+    onHide(() => {
+        console.log('App Hide')
+    })
 </script>
 
 <style lang="scss">
-// 字体图标
-@import '@/styles/fonts.scss';
+    // 字体图标
+    @import '@/styles/fonts.scss';
 
-view,
-navigator,
-input,
-scroll-view {
-  box-sizing: border-box;
-}
+    view,
+    navigator,
+    input,
+    scroll-view {
+        box-sizing: border-box;
+    }
 
-button::after {
-  border: none;
-}
+    button::after {
+        border: none;
+    }
 
-swiper,
-scroll-view {
-  flex: 1;
-  height: 100%;
-  overflow: hidden;
-}
+    swiper,
+    scroll-view {
+        flex: 1;
+        height: 100%;
+        overflow: hidden;
+    }
 
-image {
-  width: 100%;
-  height: 100%;
-  vertical-align: middle;
-}
+    image {
+        width: 100%;
+        height: 100%;
+        vertical-align: middle;
+    }
 
-// 两行省略
-.ellipsis {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-}
+    // 两行省略
+    .ellipsis {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
 </style>
--- src/pages/index/components/CustomNavbar.vue
+++ src/pages/index/components/CustomNavbar.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
+    import { computed, ref } from 'vue'
+
+    const searchValue = ref<string>()
+
     const { safeAreaInsets } = uni.getSystemInfoSync()
+    const safeAreaTop = computed(() => {
+        const top: number = safeAreaInsets?.top ?? 0 + 12
+        return top + 'px'
+    })
 </script>
 
 <template>
-    <view class="navbar" :style="{ paddingTop: safeAreaInsets?.top + 'px' }">
+    <view class="navbar" :style="{ paddingTop: safeAreaTop }">
         <!-- logo文字 -->
-        <view class="logo">
-            <image class="logo-image" src="@/static/images/logo.png"></image>
-            <text class="logo-text">新鲜 · 亲民 · 快捷</text>
-        </view>
-        <!-- 搜索条 -->
-        <view class="search">
-            <text class="icon-search">搜索商品</text>
-            <text class="icon-scan"></text>
+        <view class="space">
+            <uni-icons class="logo" type="pyq" size="30" />
+            <uni-search-bar
+                class="search"
+                v-model="searchValue"
+                :focus="true"
+                cancelButton="none"
+            />
+            <view class="place" />
         </view>
     </view>
 </template>
@@ -20,57 +29,32 @@
 <style lang="scss">
     /* 自定义导航条 */
     .navbar {
-        background-image: url(@/static/images/navigator_bg.png);
-        background-size: cover;
         position: relative;
-        display: flex;
-        flex-direction: column;
         padding-top: 20px;
+        padding-bottom: 12px;
 
-        .logo {
+        .space {
             display: flex;
             align-items: center;
             height: 64rpx;
             padding-left: 30rpx;
+            gap: 16rpx;
 
-            .logo-image {
-                width: 166rpx;
-                height: 39rpx;
+            .logo {
+                width: 32rpx;
+
+                text {
+                    color: $uni-color-primary !important;
+                }
             }
 
-            .logo-text {
+            .search {
                 flex: 1;
-                line-height: 28rpx;
-                color: #fff;
-                margin: 2rpx 0 0 20rpx;
-                padding-left: 20rpx;
-                border-left: 1rpx solid #fff;
-                font-size: 26rpx;
             }
-        }
 
-        .search {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: 0 10rpx 0 26rpx;
-            height: 64rpx;
-            margin: 16rpx 20rpx;
-            color: #fff;
-            font-size: 28rpx;
-            border-radius: 32rpx;
-            background-color: rgba(255, 255, 255, 0.5);
-        }
-
-        .icon-search {
-            &::before {
-                margin-right: 10rpx;
+            .place {
+                width: 168rpx;
             }
-        }
-
-        .icon-scan {
-            font-size: 30rpx;
-            padding: 15rpx;
         }
     }
 </style>
